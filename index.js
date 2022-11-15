2ced0a54b9172b0438b2ff50cc1d847ed6a32a2f
bakhler <thehenridit@gmail.com>
2022-11-15 09:04:13 +0200

    Add while

---
+++ index.js
@@ -0,0 +1,41 @@
+// const userNumber = " 1111";
+// let count = 0;
+// while (true) {
+//   const result = prompt("Enter number");
+//   count++;
+//   if (count > 3) {
+//     console.log("Error");
+//     break;
+//   }
+//   if (result === userNumber) {
+//     console.log("Welcome");
+//     break;
+//   }
+// }
+// console.log(count);
+
+const userNumber1 =15 ;
+const userNumber2 =65 ;
+const userNumber3 =6 ;
+userNumber===
+while (true) {
+    const result=prompt('Enter userNumber');
+    if (userNumber===userNumber1,userNumber2, userNumber3)
+    if (userNumber1>15 && userNumber2<65 && userNumber3%6 &&userNumber===result)
+  if (result === userNumber) {
+    console.log("Welcome");
+    break;
+  }
+}
+console.log(count);
+
+// for (let i = 0; i < 4; i++) {
+//   const result = prompt("enter pin");
+//   if (result === PIN_CODE) {
+//     console.log("welcome");
+//     break;
+//   }
+//   if (i === 3) {
+//     console.log("error");
+//   }
+// }
